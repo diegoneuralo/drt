5fa30a24d5cabedb04aad7cc8f72dc3b616ac4d2
diegoneuralo <diegoneuralo@gmail.com>
2012-10-18 16:50:35 -0300

    segundo commit

---
+++ src/test/java/com/defesa/drt/test/MemberRegistrationTest.java
@@ -0,0 +1,51 @@
+package com.defesa.drt.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import com.defesa.drt.controller.MemberController;
+import com.defesa.drt.model.Member;
+import com.defesa.drt.service.MemberRegistration;
+import com.defesa.drt.util.Resources;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class MemberRegistrationTest {
+   @Deployment
+   public static Archive<?> createTestArchive() {
+      return ShrinkWrap.create(WebArchive.class, "test.war")
+            .addClasses(Member.class, MemberRegistration.class, Resources.class)
+            .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+            // Deploy our test datasource
+            .addAsWebInfResource("test-ds.xml", "test-ds.xml");
+   }
+
+   @Inject
+   MemberRegistration memberRegistration;
+
+   @Inject
+   Logger log;
+
+   @Test
+   public void testRegister() throws Exception {
+      Member newMember = new Member();
+      newMember.setName("Jane Doe");
+      newMember.setEmail("jane@mailinator.com");
+      newMember.setPhoneNumber("2125551234");
+      memberRegistration.register(newMember);
+      assertNotNull(newMember.getId());
+      log.info(newMember.getName() + " was persisted with id " + newMember.getId());
+   }
+   
+}
